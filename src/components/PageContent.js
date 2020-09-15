d151589649ede84d5eec6784a657642e2748d68e
Unknown <yonk@wanadoo.fr>
2020-09-15 14:41:15 +0200

    Api testing from client

---
+++ src/components/PageContent.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import fetch from 'isomorphic-unfetch';
+
+import ITEM_CONST from '../constants/itemConstant';
+import Item from '../data/model/Item';
+import ItemComponent from './item/ItemComponent';
+import { PAINTING_URL } from '../data/api/urls';
+
+const PageContent = ({ list }) => {
+    return (
+        <>
+            {list.map((row) => {
+                const item = new Item(row, ITEM_CONST.PAINTING.KEY);
+                return <ItemComponent key={row.id} item={item} />;
+            })}
+        </>
+    );
+};
+
+PageContent.getInitialProps = async () => {
+    try {
+        const res = await fetch(PAINTING_URL);
+        const list = await res.json();
+        return { list };
+    } catch (error) {
+        if (error.response.status === 404) return { statusCode: 404 };
+        return { error };
+    }
+};
+
+PageContent.propTypes = {
+    list: PropTypes.array,
+};
+
+export default PageContent;
