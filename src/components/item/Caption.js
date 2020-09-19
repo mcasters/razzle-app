2dbd9e0b7a6a8a0e2a3fe78df42839b5362141b3
Unknown <yonk@wanadoo.fr>
2020-09-20 00:44:53 +0200

    Caption component

---
+++ src/components/item/Caption.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import s from './Caption.module.css';
+import GLOBAL_CONST from '../../constants/globalConstant';
+import CustomDate from '../../data/model/CustomDate';
+
+const Caption = ({ item }) => {
+    const date = new CustomDate(item.get('date'));
+
+    return (
+        <>
+            <figcaption>
+                <time dateTime={date.getNumberDate()} className={s.noWrap}>
+                    {date.getNumberDate()}
+                </time>
+                <span className={s.spacer}> | </span>
+                <p className={s.noWrap}>{item.get('technique')}</p>
+                <span className={s.spacer}> | </span>
+                <p className={s.noWrap}>
+                    {item.get('height')} x {item.get('width')}
+                    {item.has('length') && ` x ${item.get('length')}`} cm
+                </p>
+            </figcaption>
+            <address className={s.email}>{GLOBAL_CONST.EMAIL}</address>
+        </>
+    );
+};
+
+Caption.propTypes = {
+    item: PropTypes.object.isRequired,
+};
+
+export default Caption;
--- src/components/item/ItemComponent.js
+++ src/components/item/ItemComponent.js
@@ -1,14 +1,11 @@
-/* eslint-disable react/prop-types */
 import React from 'react';
 import PropTypes from 'prop-types';
 
 import s from './ItemComponent.module.css';
 import Images from './Images';
-import CustomDate from '../../data/model/CustomDate';
-import GLOBAL_CONST from '../../constants/globalConstant';
+import Caption from './Caption';
 
 const ItemComponent = ({ item, handleImageChange }) => {
-    const date = new CustomDate(item.get('date'));
     return (
         <article className={s.itemContainer}>
             <h2 className={s.itemTitle}>
@@ -19,19 +16,7 @@ const ItemComponent = ({ item, handleImageChange }) => {
                 alt={item.getAlt()}
                 handleImageChange={handleImageChange}
             />
-            <figcaption>
-                <time dateTime={date.getNumberDate()} className={s.noWrap}>
-                    {date.getNumberDate()}
-                </time>
-                <span className={s.spacer}> | </span>
-                <p className={s.noWrap}>{item.get('technique')}</p>
-                <span className={s.spacer}> | </span>
-                <p className={s.noWrap}>
-                    {item.get('height')} x {item.get('width')}
-                    {item.has('length') && ` x ${item.get('length')}`} cm
-                </p>
-            </figcaption>
-            <address className={s.email}>{GLOBAL_CONST.EMAIL}</address>
+            <Caption item={item} />
         </article>
     );
 };
